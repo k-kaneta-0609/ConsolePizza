463855239712358e3135a096c22937d723e4bd8e
k-kaneta-0609 <k_kaneta.10df8ca@icloud.com>
2021-07-31 14:51:47 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsolePizza/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsolePizza
+{
+    public class Employee
+    {
+        public Employee(long id, long boss)
+        {
+            this.Members = new List<long>();
+            this.ID = id;
+            this.Boss = boss;
+        }
+
+        public long ID { get; private set; }
+
+        public long Boss { get; private set; }
+
+        public List<long> Members { get; private set; }
+
+        public void SetMembers(List<Employee> employees)
+        {
+            foreach (Employee i in employees)
+            {
+                if (i.ID != this.ID)
+                {
+                    Employee emp = i;
+                    while (emp is not null && 0 < emp.Boss)
+                    {
+                        if (emp.Boss == this.ID)
+                        {
+                            this.Members.Add(i.ID);
+                            break;
+                        }
+                        else
+                        {
+                            emp = employees.FirstOrDefault(e => e.ID == emp.Boss);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsolePizza/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsolePizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long members = long.Parse(Console.ReadLine());
+            string[] boss = Console.ReadLine().Split(' ');
+            List<Employee> empList = new() { new Employee(1, 0) };
+            for (long i = 2; i <= members; i++)
+            {
+                if ((i - 1) <= (long)boss.Length)
+                {
+                    empList.Add(new Employee(i, long.Parse(boss[i - 2]))); 
+                }
+                else
+                {
+                    empList.Add(new Employee(i, 0));
+                }
+            }
+            empList.ForEach(e => e.SetMembers(empList));
+            empList.ForEach(e => Console.WriteLine(e.Members.Count.ToString()));
+        }
+    }
+}
